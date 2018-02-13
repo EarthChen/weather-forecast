ce3e3f174ae8c5a5c5facd1e31cf36954f762952
EarthChen <179153809@qq.com>
2018-02-13 13:59:24 +0800

    集成erueka服务端

---
+++ micro-weather-eureka-server/src/main/java/com/earthchen/spring/cloud/weather/WeatherEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.earthchen.spring.cloud.weather;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class WeatherEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherEurekaServerApplication.class, args);
+    }
+}
